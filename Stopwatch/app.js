de154dada07a744ce980234d0ba3838fa02faf7c
JIWONKIM <qhsh866@gmail.com>
2021-10-11 21:03:14 +0900

    feat: make functions of counter

--- Stopwatch/app.js
+++ Stopwatch/app.js
@@ -1,7 +1,39 @@
 const $display = document.querySelector('.display');
 const $stopwatch = document.querySelector('.stopwatch');
 
-$stopwatch.onclick = e => {
-  if (!e.target.classList.contains('control')) return;
-  console.log(e.target);
+const [$leftButton, $rightButton] = document.querySelectorAll('.control');
+
+let timerState = true;
+let [min, sec, mm] = [0, 0, 0];
+
+const startTime = () => {
+  mm++;
+
+  if (mm > 99) {
+    sec += 1;
+    mm = 0;
+  }
+
+  if (sec > 59) {
+    min += 1;
+    sec = 0;
+  }
+  $display.textContent = `${min < 10 ? '0' + min : min}:${
+    sec < 10 ? '0' + sec : sec
+  }:${mm < 10 ? '0' + mm : mm}`;
+};
+
+let stopWatch = null;
+
+$leftButton.onclick = () => {
+  $leftButton.textContent = timerState ? 'Start' : 'Stop';
+  $rightButton.textContent = timerState ? 'Reset' : 'Lap';
+
+  if ($leftButton.firstChild.nodeValue === 'Start') {
+    stopWatch = setInterval(startTime, 10);
+    timerState = !timerState;
+  } else {
+    clearInterval(stopWatch);
+    timerState = !timerState;
+  }
 };
