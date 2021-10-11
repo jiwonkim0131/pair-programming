56f31adfe0e8958ff393eb60c3d31f0d802b29f6
JIWONKIM <qhsh866@gmail.com>
2021-10-11 15:11:59 +0900

    feat: add app.js of toggle side nav

--- ToggleSideNav/app.js
+++ ToggleSideNav/app.js
@@ -1,1 +1,33 @@
 // toggle side nav
+const $nav = document.querySelector('nav');
+const $main = document.querySelector('main');
+const $toggleButton = document.querySelector('i.toggle');
+
+const toggleClass = $node => $node.classList.toggle('active');
+
+localStorage.setItem('navState', 'false');
+
+const fetchNav = () => {
+  //   localStorage.setItem('navState', 'false');
+  if (localStorage.getItem('navState')) {
+    $nav.classList.add('active');
+    $nav.classList.add('notransition');
+    $main.classList.add('notransition');
+    $toggleButton.classList.add('notransition');
+  } else {
+    localStorage.setItem('navState', 'false');
+  }
+};
+
+window.addEventListener('DOMContentLoaded', fetchNav);
+
+$toggleButton.onclick = () => {
+  toggleClass($nav);
+  $nav.classList.remove('notransition');
+  $main.classList.remove('notransition');
+  $toggleButton.classList.remove('notransition');
+
+  $nav.classList.contains('active')
+    ? localStorage.setItem('navState', 'false')
+    : localStorage.setItem('navState', 'true');
+};
