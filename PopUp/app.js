fedd7b206f80ac6f2b3de59d89ac7e7f9beb1413
barum_park <barumming@gmail.com>
2021-10-12 14:35:43 +0900

    feat: complete modal popup

--- PopUp/app.js
+++ PopUp/app.js
@@ -1,6 +1,6 @@
 const $togglePopUp = document.querySelector('.toggle-popup');
 const $modalWrap = document.querySelector('.modal-wrap');
-// const $form = document.querySelector('form');
+const $form = document.querySelector('form');
 const $input = document.querySelector('input');
 
 const $popupMessage = document.querySelector('.popup-message');
@@ -9,11 +9,19 @@ $togglePopUp.onclick = () => {
   $modalWrap.classList.toggle('active');
 };
 
-$input.onkeyup = e => {
+$form.onsubmit = e => {
   e.preventDefault();
-  if (e.key !== 'Enter') return;
   const content = $input.value.trim();
+
   if (content) {
     $popupMessage.textContent = content;
+    $input.value = '';
+    $modalWrap.classList.remove('active');
   }
+};
+
+$modalWrap.onclick = e => {
+  if (!e.target.classList.contains('close')) return;
+  $modalWrap.classList.remove('active');
+  if ($input.value) $input.value = '';
 };
