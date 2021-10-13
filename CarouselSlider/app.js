9b855577649c1e8ca50c3beca01099b82514bac2
JIWONKIM <qhsh866@gmail.com>
2021-10-13 15:19:33 +0900

    feat: add eventhandler for carousel control

--- CarouselSlider/app.js
+++ CarouselSlider/app.js
@@ -4,33 +4,76 @@ const carousel = ($container, images) => {
   const $carouselSlides = document.createElement('div');
   $carouselSlides.classList.add('carousel-slides');
   $container.appendChild($carouselSlides);
-  $carouselSlides.innerHTML = images.map(img => `<img src="${img}">`).join('');
+  $carouselSlides.innerHTML = images
+    .map(img => `<img src="${img}" style='width : 100%'>`)
+    .join('');
+  $carouselSlides.style.setProperty('--currentSlide', 1);
 };
 
 carousel($carousel, [
+  'movies/movie-4.jpg',
   'movies/movie-1.jpg',
   'movies/movie-2.jpg',
   'movies/movie-3.jpg',
-  'movies/movie-4.jpg'
+  'movies/movie-4.jpg',
+  'movies/movie-1.jpg'
 ]);
+
+const imageWidth = document.querySelector('img').naturalWidth;
+$carousel.style.maxWidth = `${imageWidth}px`;
 
 const $fragment = document.createDocumentFragment();
 
-['&laquo;', '&raquo;'].forEach(text => {
+const buttonObj = [
+  { key: 'prev', val: '&laquo' },
+  { key: 'next', val: '&raquo' }
+];
+
+buttonObj.forEach(({ key, val }) => {
   const $carouselControl = document.createElement('button');
-  $carouselControl.classList.add('carousel-control');
-  const textNode = document.createTextNode(`${text}`);
-  $carouselControl.appendChild(textNode);
+  $carouselControl.classList.add('carousel-control', key);
+  $carouselControl.innerHTML = val;
   $fragment.appendChild($carouselControl);
 });
 
 $carousel.appendChild($fragment);
 
-// const $carouselControlNext = document.createElement('button');
+const $currentSlides = document.querySelector('.carousel-slides');
 
-// const nextText = document.createTextNode('&raquo');
-// $carouselControlPrev.appendChild(prevText);
-// $carouselControlNext.appendChild(nextText);
+let currentSlide =
+  getComputedStyle($currentSlides).getPropertyValue('--currentSlide') * 1;
 
-// $carouselControlPrev.classList.add('carousel-control', 'prev');
-// $carouselControlNext.classList.add('carousel-control', 'next');
+$currentSlides.style.setProperty('--duration', 500);
+
+$carousel.onclick = e => {
+  if (!e.target.classList.contains('carousel-control')) return;
+
+  if (e.target.classList.contains('prev')) {
+    currentSlide -= 1;
+    if (!currentSlide) {
+      setTimeout(() => {
+        $currentSlides.style.setProperty('--duration', 0);
+        $currentSlides.style.setProperty(
+          '--currentSlide',
+          $currentSlides.children.length - 2
+        );
+        currentSlide = $currentSlides.children.length - 2;
+      }, $currentSlides.style.getPropertyValue('--duration'));
+    }
+    $currentSlides.style.setProperty('--duration', 500);
+    $currentSlides.style.setProperty('--currentSlide', currentSlide);
+  }
+
+  if (e.target.classList.contains('next')) {
+    currentSlide += 1;
+    if (currentSlide === $currentSlides.children.length - 1) {
+      setTimeout(() => {
+        $currentSlides.style.setProperty('--duration', 0);
+        $currentSlides.style.setProperty('--currentSlide', 1);
+        currentSlide = 1;
+      }, $currentSlides.style.getPropertyValue('--duration'));
+    }
+    $currentSlides.style.setProperty('--currentSlide', currentSlide);
+    $currentSlides.style.setProperty('--duration', 500);
+  }
+};
