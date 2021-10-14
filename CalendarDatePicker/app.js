b0fa3194d1273199c0b7b81ed2b6cd133f79aed6
JIWONKIM <qhsh866@gmail.com>
2021-10-14 13:24:40 +0900

    feat: Make getAllDates function of calendar

--- CalendarDatePicker/app.js
+++ CalendarDatePicker/app.js
@@ -1,40 +1,58 @@
 const $calendar = document.querySelector('.calendar');
 const DAYS = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
-// dummy data
-// const beforeDays = [26, 27, 28, 29, 30];
-const thisDays = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31
-];
-// const afterDays = [1, 2, 3, 4, 5, 6];
-
-const currentDate = () => {
+const currentDate = (() => {
   const current = new Date();
   const year = current.getFullYear();
   const month = current.getMonth();
   const date = current.getDate();
 
-  return { current, year, month, date };
+  return { year, month, date };
+})();
+
+// 요일 구하기
+const getDay = (year, month) => new Date(year, month, 1).getDay();
+
+// 말일 구하기
+const getLastDate = (year, month) => new Date(year, month + 1, 0).getDate();
+
+// 이전달, 이번달, 다음달 렌더링 할 배열 구하기
+const getAllDates = currentDate => {
+  const { year, month } = currentDate; // 기준
+
+  // 이번달 1일의 요일
+  const firstDayofThisMonth = getDay(year, month);
+
+  // 지난달의 말일
+  const lastDateofLastMonth = getLastDate(year, month - 1);
+
+  // 이번달의 말일
+  const lastDateofThisMonth = getLastDate(year, month);
+
+  // 지난 달 날짜 배열 구하기
+  const lastMonthDays = Array.from(
+    { length: firstDayofThisMonth },
+    (_, i) => lastDateofLastMonth - i - 1
+  ).reverse();
+
+  // 이번 달 날짜 배열 구하기
+  const currentMonthDays = Array.from(
+    { length: lastDateofThisMonth },
+    (_, i) => i + 1
+  );
+
+  // 다음 날 시작 날짜 배열 구하기
+  const nextMonthDays = Array.from(
+    { length: 6 - lastDateofThisMonth },
+    (_, i) => i + 1
+  );
+
+  return { lastMonthDays, currentMonthDays, nextMonthDays };
 };
 
-const getDate = currentDate();
-const getLasteDate = (year, month) => new Date(year, month + 1, 0).getDate();
-const getFirstDay = (year, month) => new Date(year, month, 1).getDay();
-const getLastDay = (year, month) => new Date(year, month + 1, 0).getDay();
-const getCurrentDay = getDate.current.getDay();
-const lasteDateThisMonth = getLastDay(getDate.year, getDate.month);
-
-const beforeDays = Array.from(
-  { length: getFirstDay(getDate.year, getDate.month) },
-  (_, i) => getLasteDate(getDate.year, getDate.month) - i - 1
-).reverse();
-
-const afterDays = Array.from(
-  { length: 7 - lasteDateThisMonth - 1 },
-  (_, i) => i + 1
-);
 const render = () => {
+  const { lastMonthDays, currentMonthDays, nextMonthDays } =
+    getAllDates(currentDate);
   const $calendarNav = document.createElement('div');
   $calendarNav.classList.add('calendar-nav');
 
@@ -56,21 +74,21 @@ const render = () => {
     $fragment.append($day);
   });
 
-  beforeDays.forEach(beforeDay => {
+  lastMonthDays.forEach(beforeDay => {
     const $beforeDay = document.createElement('div');
     $beforeDay.classList.add('date', 'disable');
     $beforeDay.textContent = beforeDay;
     $fragment.append($beforeDay);
   });
 
-  thisDays.forEach(thisDay => {
+  currentMonthDays.forEach(thisDay => {
     const $thisDay = document.createElement('div');
     $thisDay.classList.add('date');
     $thisDay.textContent = thisDay;
     $fragment.append($thisDay);
   });
 
-  afterDays.forEach(afterDay => {
+  nextMonthDays.forEach(afterDay => {
     const $afterDay = document.createElement('div');
     $afterDay.classList.add('date', 'disable');
     $afterDay.textContent = afterDay;
