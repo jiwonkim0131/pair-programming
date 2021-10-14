63a6050dd886af991085b727f04f9e2ad90b036b
JIWONKIM <qhsh866@gmail.com>
2021-10-14 15:26:53 +0900

    feat: add event handler fot calendar click event

--- CalendarDatePicker/app.js
+++ CalendarDatePicker/app.js
@@ -1,5 +1,20 @@
 const $calendar = document.querySelector('.calendar');
+const $datePicker = document.querySelector('.date-picker');
 const DAYS = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+const MONTH = [
+  'JAN',
+  'FEB',
+  'MAR',
+  'APR',
+  'MAY',
+  'JUN',
+  'JUL',
+  'AUG',
+  'SEP',
+  'OCT',
+  'NOV',
+  'DEC'
+];
 
 const currentDate = (() => {
   const current = new Date();
@@ -9,6 +24,18 @@ const currentDate = (() => {
 
   return { year, month, date };
 })();
+
+const nowDate = {
+  year: currentDate.year,
+  month: currentDate.month,
+  nowSelected: 0
+};
+
+const updateNowDate = (year, month, selected = 0) => {
+  nowDate.year = year;
+  nowDate.month = month;
+  nowDate.nowSelected = selected;
+};
 
 // today 함수
 const isToday = date => date === currentDate.date;
@@ -20,40 +47,48 @@ const getDayNum = (year, month) => new Date(year, month, 1).getDay();
 const getLastDate = (year, month) => new Date(year, month + 1, 0).getDate();
 
 // 말일의 요일 구하기
-const getLasetDayofMonth = (year, month) =>
+const getLastDayOfMonth = (year, month) =>
   new Date(year, month + 1, 0).getDay();
 
+// 출력용 data formatting
+const getFormattedDate = date => {
+  const year = date.getFullYear();
+  const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
+  return year + '-' + month + '-' + day;
+};
+
 // 이전달, 이번달, 다음달 렌더링 할 배열 구하기
-const getAllDates = currentDate => {
-  const { year, month } = currentDate; // 기준
+const getAllDates = nowDate => {
+  const { year, month } = nowDate; // 기준
 
   // 이번달 1일의 요일
-  const firstDayofThisMonth = getDayNum(year, month);
+  const firstDayOfThisMonth = getDayNum(year, month);
 
   // 지난달의 말일
-  const lastDateofLastMonth = getLastDate(year, month - 1);
+  const lastDateOfLastMonth = getLastDate(year, month - 1);
 
   // 이번달의 말일
-  const lastDateofThisMonth = getLastDate(year, month);
+  const lastDateOfThisMonth = getLastDate(year, month);
 
   // 이번달 말일의 요일
-  const lastDayofThisMonth = getLasetDayofMonth(year, month);
+  const lastDayOfThisMonth = getLastDayOfMonth(year, month);
 
   // 지난 달 날짜 배열 구하기
   const lastMonthDays = Array.from(
-    { length: firstDayofThisMonth },
-    (_, i) => lastDateofLastMonth - i - 1
+    { length: firstDayOfThisMonth },
+    (_, i) => lastDateOfLastMonth - i - 1
   ).reverse();
 
   // 이번 달 날짜 배열 구하기
   const currentMonthDays = Array.from(
-    { length: lastDateofThisMonth },
+    { length: lastDateOfThisMonth },
     (_, i) => i + 1
   );
 
   // 다음 날 시작 날짜 배열 구하기
   const nextMonthDays = Array.from(
-    { length: 6 - lastDayofThisMonth },
+    { length: 6 - lastDayOfThisMonth },
     (_, i) => i + 1
   );
 
@@ -62,14 +97,15 @@ const getAllDates = currentDate => {
 
 const render = () => {
   const { lastMonthDays, currentMonthDays, nextMonthDays } =
-    getAllDates(currentDate);
+    getAllDates(nowDate);
   const $calendarNav = document.createElement('div');
+
   $calendarNav.classList.add('calendar-nav');
 
   $calendarNav.innerHTML = ` <button type="button" class="nav-button prev">◀</button>
   <div class="calendar-title">
-    <p class="title-month">OCT</p>
-    <p class="title-year">2021</p>
+    <p class="title-month">${MONTH[nowDate.month]}</p>
+    <p class="title-year">${nowDate.year}</p>
   </div>
   <button type="button" class="nav-button next">▶</button>`;
 
@@ -97,6 +133,10 @@ const render = () => {
       ? $thisDay.classList.add('date', 'today')
       : $thisDay.classList.add('date');
     $thisDay.textContent = thisDay;
+    if (thisDay === nowDate.nowSelected) {
+      console.log(nowDate.nowSelected);
+      $thisDay.classList.add('selected');
+    }
     $fragment.append($thisDay);
   });
 
@@ -114,17 +154,44 @@ const render = () => {
 
 render();
 
-let selected = 0;
 // select 이벤트
 $calendar.onclick = e => {
-  if (
-    !e.target.classList.contains('date') ||
-    e.target.classList.contains('disable')
-  )
-    return;
-  [...$calendar.querySelectorAll('.date')].forEach($el =>
-    $el.classList.toggle('selected', $el === e.target)
-  );
+  if (e.target.classList.contains('disable')) return;
 
-  selected = e.target.textContent;
+  if (e.target.classList.contains('date')) {
+    [...$calendar.querySelectorAll('.date')].forEach($el =>
+      $el.classList.toggle('selected', $el === e.target)
+    );
+
+    // date picker 날짜 출력
+    const year = $calendar.querySelector('.title-year').textContent * 1;
+    const month =
+      MONTH.indexOf($calendar.querySelector('.title-month').textContent) * 1;
+    const date = e.target.textContent * 1;
+
+    updateNowDate(year, month, date);
+
+    $datePicker.value = getFormattedDate(new Date(year, month, date));
+  }
+
+  if (e.target.classList.contains('nav-button')) {
+    $calendar.innerHTML = '';
+    if (e.target.classList.contains('prev')) {
+      nowDate.month === 0
+        ? updateNowDate(nowDate.year - 1, 11, nowDate.nowSelected)
+        : updateNowDate(nowDate.year, nowDate.month - 1, nowDate.nowSelected);
+      $datePicker.value = getFormattedDate(
+        new Date(nowDate.year, nowDate.month, nowDate.nowSelected)
+      );
+      render();
+    } else {
+      nowDate.month === 11
+        ? updateNowDate(nowDate.year + 1, 0, nowDate.nowSelected)
+        : updateNowDate(nowDate.year, nowDate.month + 1, nowDate.nowSelected);
+      $datePicker.value = getFormattedDate(
+        new Date(nowDate.year, nowDate.month, nowDate.nowSelected)
+      );
+      render();
+    }
+  }
 };
