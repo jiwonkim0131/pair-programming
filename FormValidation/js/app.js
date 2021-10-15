270d560d8e4682b9ca7e76405dd7cc337486e5e6
JIWONKIM <qhsh866@gmail.com>
2021-10-15 22:29:41 +0900

    refactor: Refactoring Formvalidation
    make inputStatus object, isSubmit&isSamePassword function

--- FormValidation/js/app.js
+++ FormValidation/js/app.js
@@ -1,238 +1,71 @@
-// toaster
-const $body = document.querySelector('body');
+import { toaster, createToastAction } from './toast.js';
 
-const toaster = {
-  add(callback) {
-    const $toast = document.createElement('div');
-    $toast.classList.add('toast', 'toast-' + callback.type);
-    $body.appendChild($toast);
-    $body.style.overflowX = 'hidden';
-
-    const $toastList = document.querySelectorAll('.toast');
-
-    $toast.innerHTML = `
-      <h4 class="toast-heading">${callback.title} ${$toastList.length - 1}</h4>
-      <div class="toast-message">
-        <svg width="24" height="24">
-          <use xlink:href="#${callback.type}" />
-        </svg>
-        <p>${callback.message}</p>
-      </div>
-      <a class="close">&times;</a>
-    `;
-
-    const height = $toast.offsetHeight;
-
-    [...$toastList].reverse().forEach((toast, idx) => {
-      toast.style.bottom = `${idx * height}px`;
-    });
-
-    setTimeout(() => $toast.remove(), 3000);
+const inputStatus = {
+  userid: {
+    RegExp:
+      /^[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
+    errorMessage: '올바른 아이디 형식을 입력하세요.',
+    status: false
+  },
+  password: {
+    RegExp: /^[A-Za-z0-9]{6,12}$/,
+    errorMessage: '영문 또는 숫자를 6-12자 입력하세요.',
+    status: false
+  },
+  username: {
+    RegExp: /(.)/,
+    errorMessage: '이름을 입력해주세요.',
+    status: false
+  },
+  'confirm-password': {
+    errorMessage: '패스워드가 일치하지 않습니다.',
+    status: false
   }
 };
 
-const createToastAction = (type, title, message) => ({ type, title, message });
+const isSubmit = inputAll =>
+  [...inputAll].every(inputType => inputStatus[inputType.name].status);
+const isSamePassword = confirmPassword =>
+  confirmPassword === $signupPassword.value;
 
-// signIn
 const $signinForm = document.querySelector('.form.signin');
 const $signupForm = document.querySelector('.form.signup');
-const $signinIcons = document.querySelectorAll('.form.signin .icon');
-const $signupIcons = document.querySelectorAll('.form.signup .icon');
-const $signinError = document.querySelectorAll('.form.signin .error');
-const $signupError = document.querySelectorAll('.form.signup .error');
-const $signinButton = document.querySelector('button.signin');
-const $signupButton = document.querySelector('button.signup');
-const $signinUserid = document.getElementById('signin-userid');
-const $signinPassword = document.getElementById('signin-password');
+const $signupPassword = document.getElementById('signup-password');
 const $signinLink = document.querySelector('.form.signup .link>a');
 const $signupLink = document.querySelector('.form.signin .link>a');
 
-const signInState = {
-  userid: false,
-  password: false
-};
+[$signinForm, $signupForm].forEach($form => {
+  $form.oninput = e => {
+    const $iconSuccess = e.target.parentNode.querySelector('.icon-success');
+    const inputAll = $form.querySelectorAll('input');
+    const $iconError = e.target.parentNode.querySelector('.icon-error');
+    const $errorMessage = e.target.parentNode.querySelector('.error');
+    const inputName = inputStatus[e.target.name];
 
-const checkId = checkVal => {
-  let idState = false;
-  idState =
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/.test(
-      checkVal
+    inputName.status =
+      e.target.name !== 'confirm-password'
+        ? inputName.RegExp.test(e.target.value)
+        : isSamePassword(e.target.value);
+
+    $iconSuccess.classList.toggle('hidden', !inputName.status);
+    $iconError.classList.toggle('hidden', inputName.status);
+    $errorMessage.textContent = inputName.status ? '' : inputName.errorMessage;
+
+    e.target.closest('.form').querySelector('.button').disabled =
+      !isSubmit(inputAll);
+  };
+
+  $form.onsubmit = e => {
+    e.preventDefault();
+    toaster.add(
+      createToastAction('success', 'Well done!', 'This is a success alert')
     );
-  if (idState) {
-    $signinIcons[0].classList.remove('hidden');
-    $signinIcons[1].classList.add('hidden');
-    $signinError[0].textContent = '';
-  } else {
-    $signinIcons[0].classList.add('hidden');
-    $signinIcons[1].classList.remove('hidden');
-    $signinError[0].textContent = '올바른 아이디 형식을 입력하세요';
-  }
-  return idState;
-};
+  };
+});
 
-const checkPwd = checkVal => {
-  let passwordState = false;
-
-  passwordState = /^[A-Za-z0-9]{6,12}$/.test(checkVal);
-  if (passwordState) {
-    $signinIcons[2].classList.remove('hidden');
-    $signinIcons[3].classList.add('hidden');
-    $signinError[1].textContent = '';
-  } else {
-    $signinIcons[2].classList.add('hidden');
-    $signinIcons[3].classList.remove('hidden');
-    $signinError[1].textContent = '영문 또는 숫자를 6~12자 입력하세요';
-  }
-  return passwordState;
-};
-
-$signinUserid.onkeyup = e => {
-  signInState[e.target.name] = checkId(e.target.value);
-};
-
-$signinPassword.onkeyup = e => {
-  signInState[e.target.name] = checkPwd(e.target.value);
-};
-
-$signinForm.onkeyup = () => {
-  if (signInState.userid && signInState.password)
-    $signinButton.disabled = false;
-  else {
-    $signinButton.disabled = true;
-  }
-};
-
-$signinForm.onsubmit = e => {
-  e.preventDefault();
-  toaster.add(
-    createToastAction('success', 'Well done!', 'This is a success alert')
-  );
-  console.log($signinUserid.value);
-  console.log($signinPassword.value);
-};
-
-$signinLink.onclick = () => {
-  $signupForm.classList.add('hidden');
-  $signinForm.classList.remove('hidden');
-};
-
-// signUp
-const $signupUserid = document.getElementById('signup-userid');
-const $signupName = document.getElementById('signup-name');
-const $signupPassword = document.getElementById('signup-password');
-const $signupConfirmPassword = document.getElementById(
-  'signup-confirm-password'
-);
-
-const signUpState = {
-  userid: false,
-  password: false,
-  username: false,
-  'confirm-password': false
-};
-const checkSingupId = checkVal => {
-  let idState = false;
-  idState =
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/.test(
-      checkVal
-    );
-  if (idState) {
-    $signupIcons[0].classList.remove('hidden');
-    $signupIcons[1].classList.add('hidden');
-    $signupError[0].textContent = '';
-  } else {
-    $signupIcons[0].classList.add('hidden');
-    $signupIcons[1].classList.remove('hidden');
-    $signupError[0].textContent = '올바른 아이디 형식을 입력하세요';
-  }
-  return idState;
-};
-
-const checkName = checkVal => {
-  if (checkVal) {
-    $signupIcons[2].classList.remove('hidden');
-    $signupIcons[3].classList.add('hidden');
-    $signupError[1].textContent = '';
-  } else {
-    $signupIcons[2].classList.add('hidden');
-    $signupIcons[3].classList.remove('hidden');
-    $signupError[1].textContent = '이름을 입력해주세요';
-  }
-  return !!checkVal;
-};
-
-const checkSignupPwd = checkVal => {
-  let passwordState = false;
-
-  passwordState = /^[A-Za-z0-9]{6,12}$/.test(checkVal);
-  if (passwordState) {
-    $signupIcons[4].classList.remove('hidden');
-    $signupIcons[5].classList.add('hidden');
-    $signupError[2].textContent = '';
-  } else {
-    $signupIcons[4].classList.add('hidden');
-    $signupIcons[5].classList.remove('hidden');
-    $signupError[2].textContent = '영문 또는 숫자를 6~12자 입력하세요';
-  }
-  return passwordState;
-};
-
-const checkConfirmPwd = checkVal => {
-  if (checkVal === $signupPassword.value) {
-    $signupIcons[6].classList.remove('hidden');
-    $signupIcons[7].classList.add('hidden');
-    $signupError[3].textContent = '';
-  } else {
-    $signupIcons[6].classList.add('hidden');
-    $signupIcons[7].classList.remove('hidden');
-    $signupError[3].textContent = '패스워드가 일치하지 않습니다.';
-  }
-
-  return checkVal === $signupPassword.value;
-};
-
-$signupUserid.onkeyup = e => {
-  signUpState[e.target.name] = checkSingupId(e.target.value);
-};
-
-$signupName.onkeyup = e => {
-  signUpState[e.target.name] = checkName(e.target.value.trim());
-};
-
-$signupPassword.onkeyup = e => {
-  signUpState[e.target.name] = checkSignupPwd(e.target.value);
-};
-
-$signupConfirmPassword.onkeyup = e => {
-  signUpState[e.target.name] = checkConfirmPwd(e.target.value);
-};
-
-$signupLink.onclick = () => {
-  $signinForm.classList.add('hidden');
-  $signupForm.classList.remove('hidden');
-};
-
-$signupForm.onkeyup = () => {
-  // console.log(signUpState['confirm-password']);
-  if (
-    signUpState.userid &&
-    signUpState.password &&
-    signUpState.username &&
-    signUpState['confirm-password']
-  )
-    $signupButton.disabled = false;
-  else {
-    $signupButton.disabled = true;
-  }
-};
-
-$signupForm.onsubmit = e => {
-  e.preventDefault();
-  toaster.add(
-    createToastAction('success', 'Well done!', 'This is a success alert')
-  );
-  console.log($signupUserid.value);
-  console.log($signupName.value);
-  console.log($signupPassword.value);
-  console.log($signupConfirmPassword.value);
-};
+[$signinLink, $signupLink].forEach($formLink => {
+  $formLink.onclick = () => {
+    $signinLink.closest('form').classList.toggle('hidden');
+    $signupLink.closest('form').classList.toggle('hidden');
+  };
+});
---
+++ FormValidation/js/toast.js
@@ -0,0 +1,33 @@
+const $body = document.querySelector('body');
+$body.style.overflowX = 'hidden';
+const toastQueue = [];
+
+const toaster = {
+  add(callback) {
+    const $toast = document.createElement('div');
+    $toast.classList.add('toast', 'toast-' + callback.type);
+    toastQueue.unshift($toast);
+    $body.appendChild($toast);
+
+    $toast.innerHTML = `
+      <h4 class="toast-heading">${callback.title} ${toastQueue.length - 1}</h4>
+      <div class="toast-message">
+        <svg width="24" height="24">
+          <use xlink:href="#${callback.type}" />
+        </svg>
+        <p>${callback.message}</p>
+      </div>
+      <a class="close">&times;</a>
+    `;
+
+    toastQueue.forEach((toast, idx) => {
+      toast.style.bottom = `${idx * $toast.offsetHeight}px`;
+    });
+
+    setTimeout(() => toastQueue.pop().remove(), 3000);
+  }
+};
+
+const createToastAction = (type, title, message) => ({ type, title, message });
+
+export { toaster, createToastAction };
