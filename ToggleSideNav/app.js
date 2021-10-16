bf77adb32cc7560b06e893b1e0974632b51c454b
JIWONKIM <qhsh866@gmail.com>
2021-10-16 18:18:51 +0900

    refactor: remove repeated task

--- ToggleSideNav/app.js
+++ ToggleSideNav/app.js
@@ -1,28 +1,21 @@
-// 01. toggle side nav
-
-// DOM Nodes
 const $nav = document.querySelector('nav');
 const $main = document.querySelector('main');
 const $toggleButton = document.querySelector('.toggle');
 
-// state function
 const fetchNav = () => {
-  if (JSON.parse(localStorage.getItem('navState'))) {
-    $nav.classList.add('active');
-    [$nav, $main, $toggleButton].forEach($el =>
-      $el.classList.add('notransition')
-    );
-  }
+  $nav.classList.toggle('active', JSON.parse(localStorage.getItem('navState')));
+  [$nav, $main, $toggleButton].forEach($el =>
+    $el.classList.add('notransition')
+  );
 };
 
-// Event bindings
 window.addEventListener('DOMContentLoaded', fetchNav);
-
-$toggleButton.onclick = () => {
-  $nav.classList.toggle('active');
+window.addEventListener('load', () => {
   [$nav, $main, $toggleButton].forEach($el =>
     $el.classList.remove('notransition')
   );
+});
 
-  localStorage.navState = !!$nav.classList.contains('active');
+$toggleButton.onclick = () => {
+  localStorage.navState = $nav.classList.toggle('active');
 };
