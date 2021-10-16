526cb3cb3bac0bd9aa73c162f8a400b1dc4717cb
JIWONKIM <qhsh866@gmail.com>
2021-10-16 17:22:57 +0900

    reafactor: Rename variable and Update date select function

--- CalendarDatePicker/app.js
+++ CalendarDatePicker/app.js
@@ -1,162 +1,153 @@
+// DOM Nodes ======================
 const $body = document.querySelector('body');
 const $calendar = document.querySelector('.calendar');
 const $datePicker = document.querySelector('.date-picker');
+
+// State  ======================
 const DAYS = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 const MONTH = [
-  'JAN',
-  'FEB',
-  'MAR',
-  'APR',
-  'MAY',
-  'JUN',
-  'JUL',
-  'AUG',
-  'SEP',
-  'OCT',
-  'NOV',
-  'DEC'
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
 ];
 
-const currentDate = (() => {
-  const current = new Date();
-  const year = current.getFullYear();
-  const month = current.getMonth();
-  const date = current.getDate();
+let selectedDate;
+
+// State Function  ======================
+const baseDate = (() => {
+  const base = new Date();
+  const year = base.getFullYear();
+  const month = base.getMonth();
+  const date = base.getDate();
 
   return { year, month, date };
 })();
 
-const nowDate = {
-  year: currentDate.year,
-  month: currentDate.month,
+const currentDate = {
+  year: baseDate.year,
+  month: baseDate.month,
   nowSelected: 0
 };
 
-const updateNowDate = (year, month, selected = 0) => {
-  nowDate.year = year;
-  nowDate.month = month;
-  nowDate.nowSelected = selected;
+const updateCurrentDate = (year, month, selected = 0) => {
+  currentDate.year = year;
+  currentDate.month = month;
+  currentDate.nowSelected = selected;
 };
 
-// today 함수
 const isToday = date =>
-  date === currentDate.date && currentDate.month === nowDate.month;
+  date === baseDate.date &&
+  baseDate.month === currentDate.month &&
+  baseDate.year === currentDate.year;
 
-// 요일 구하기
-const getDayNum = (year, month) => new Date(year, month, 1).getDay();
+const getFirstDay = (year, month) => new Date(year, month, 1).getDay();
 
-// 말일 구하기
 const getLastDate = (year, month) => new Date(year, month + 1, 0).getDate();
 
-// 말일의 요일 구하기
 const getLastDayOfMonth = (year, month) =>
   new Date(year, month + 1, 0).getDay();
 
-// 출력용 data formatting
-const getFormattedDate = date => {
-  const year = date.getFullYear();
-  const month = ('0' + (date.getMonth() + 1)).slice(-2);
-  const day = ('0' + date.getDate()).slice(-2);
-  return year + '-' + month + '-' + day;
-};
-
-// 이전달, 이번달, 다음달 렌더링 할 배열 구하기
 const getAllDates = nowDate => {
-  const { year, month } = nowDate; // 기준
+  const { year, month } = nowDate;
 
-  // 이번달 1일의 요일
-  const firstDayOfThisMonth = getDayNum(year, month);
-
-  // 지난달의 말일
+  const firstDayOfThisMonth = getFirstDay(year, month);
   const lastDateOfLastMonth = getLastDate(year, month - 1);
-
-  // 이번달의 말일
   const lastDateOfThisMonth = getLastDate(year, month);
-
-  // 이번달 말일의 요일
   const lastDayOfThisMonth = getLastDayOfMonth(year, month);
 
-  // 지난 달 날짜 배열 구하기
-  const lastMonthDays = Array.from(
+  const dateOfLastMonth = Array.from(
     { length: firstDayOfThisMonth },
-    (_, i) => lastDateOfLastMonth - i - 1
-  ).reverse();
-
-  // 이번 달 날짜 배열 구하기
-  const currentMonthDays = Array.from(
+    (_, i) => lastDateOfLastMonth - firstDayOfThisMonth + i + 1
+  );
+  const dateOfThisMonth = Array.from(
     { length: lastDateOfThisMonth },
     (_, i) => i + 1
   );
-
-  // 다음 날 시작 날짜 배열 구하기
-  const nextMonthDays = Array.from(
+  const dateOfNextMonth = Array.from(
     { length: 6 - lastDayOfThisMonth },
     (_, i) => i + 1
   );
 
-  return { lastMonthDays, currentMonthDays, nextMonthDays };
+  return { dateOfLastMonth, dateOfThisMonth, dateOfNextMonth };
 };
 
 const render = () => {
-  const { lastMonthDays, currentMonthDays, nextMonthDays } =
-    getAllDates(nowDate);
+  const { dateOfLastMonth, dateOfThisMonth, dateOfNextMonth } =
+    getAllDates(currentDate);
   const $calendarNav = document.createElement('div');
-
   $calendarNav.classList.add('calendar-nav');
 
   $calendarNav.innerHTML = ` <button type="button" class="nav-button prev">◀</button>
   <div class="calendar-title">
-    <p class="title-month">${MONTH[nowDate.month]}</p>
-    <p class="title-year">${nowDate.year}</p>
+    <p class="title-month">${MONTH[currentDate.month]}</p>
+    <p class="title-year">${currentDate.year}</p>
   </div>
   <button type="button" class="nav-button next">▶</button>`;
 
   const $calendarGrid = document.createElement('div');
   $calendarGrid.classList.add('calendar-grid');
 
-  const $fragment = document.createDocumentFragment();
+  const $fragmentForGrid = document.createDocumentFragment();
+
   DAYS.forEach(day => {
     const $day = document.createElement('div');
     $day.classList.add('day');
     $day.textContent = day;
-    $fragment.append($day);
+    $fragmentForGrid.append($day);
   });
 
-  lastMonthDays.forEach(beforeDay => {
+  dateOfLastMonth.forEach(date => {
     const $beforeDay = document.createElement('div');
     $beforeDay.classList.add('date', 'disable');
-    $beforeDay.textContent = beforeDay;
-    $fragment.append($beforeDay);
+    $beforeDay.textContent = date;
+    $fragmentForGrid.append($beforeDay);
   });
 
-  currentMonthDays.forEach(thisDay => {
+  dateOfThisMonth.forEach(date => {
     const $thisDay = document.createElement('div');
-    isToday(thisDay)
+    $thisDay.dataset.date = `${currentDate.year}-${
+      currentDate.month + 1 < 10
+        ? '0' + (currentDate.month + 1)
+        : currentDate.month + 1
+    }-${date < 10 ? '0' + date : date}`;
+    isToday(date)
       ? $thisDay.classList.add('date', 'today')
       : $thisDay.classList.add('date');
-    $thisDay.textContent = thisDay;
-    if (thisDay === nowDate.nowSelected) {
-      $thisDay.classList.add('selected');
-    }
-    $fragment.append($thisDay);
+    $thisDay.textContent = date;
+    $thisDay.dataset.date === selectedDate
+      ? $thisDay.classList.add('date', 'selected')
+      : $thisDay.classList.add('date');
+
+    $fragmentForGrid.append($thisDay);
   });
 
-  nextMonthDays.forEach(afterDay => {
+  dateOfNextMonth.forEach(date => {
     const $afterDay = document.createElement('div');
     $afterDay.classList.add('date', 'disable');
-    $afterDay.textContent = afterDay;
-    $fragment.append($afterDay);
+    $afterDay.textContent = date;
+    $fragmentForGrid.append($afterDay);
   });
 
-  $calendarGrid.append($fragment);
+  $calendarGrid.append($fragmentForGrid);
   $calendar.append($calendarNav);
   $calendar.append($calendarGrid);
 };
 
-render();
+// Event Bindings  ======================
+window.addEventListener('DOMContentLoaded', render);
 
-// select 이벤트
 $calendar.onclick = e => {
+  e.stopPropagation();
+
   if (e.target.classList.contains('disable')) return;
 
   if (e.target.classList.contains('date')) {
@@ -164,15 +155,15 @@ $calendar.onclick = e => {
       $el.classList.toggle('selected', $el === e.target)
     );
 
-    // date picker 날짜 출력
     const year = $calendar.querySelector('.title-year').textContent * 1;
     const month =
       MONTH.indexOf($calendar.querySelector('.title-month').textContent) * 1;
     const date = e.target.textContent * 1;
 
-    updateNowDate(year, month, date);
+    updateCurrentDate(year, month, date);
 
-    $datePicker.value = getFormattedDate(new Date(year, month, date));
+    selectedDate = e.target.dataset.date;
+    $datePicker.value = e.target.dataset.date;
     $calendar.classList.remove('active');
   }
 
@@ -180,34 +171,32 @@ $calendar.onclick = e => {
     $calendar.innerHTML = '';
 
     if (e.target.classList.contains('prev')) {
-      nowDate.month === 0
-        ? updateNowDate(nowDate.year - 1, 11, nowDate.nowSelected)
-        : updateNowDate(nowDate.year, nowDate.month - 1, nowDate.nowSelected);
+      currentDate.month === 0
+        ? updateCurrentDate(currentDate.year - 1, 11, currentDate.nowSelected)
+        : updateCurrentDate(
+            currentDate.year,
+            currentDate.month - 1,
+            currentDate.nowSelected
+          );
     } else {
-      nowDate.month === 11
-        ? updateNowDate(nowDate.year + 1, 0, nowDate.nowSelected)
-        : updateNowDate(nowDate.year, nowDate.month + 1, nowDate.nowSelected);
+      currentDate.month === 11
+        ? updateCurrentDate(currentDate.year + 1, 0, currentDate.nowSelected)
+        : updateCurrentDate(
+            currentDate.year,
+            currentDate.month + 1,
+            currentDate.nowSelected
+          );
     }
-
-    if (nowDate.nowSelected)
-      $datePicker.value = getFormattedDate(
-        new Date(nowDate.year, nowDate.month, nowDate.nowSelected)
-      );
 
     render();
   }
 };
 
-$datePicker.onclick = () => {
+$datePicker.onclick = e => {
+  e.stopPropagation();
   $calendar.classList.add('active');
 };
 
-$body.onclick = e => {
-  if (
-    e.target.classList.contains('date-picker') ||
-    e.target.matches('.calendar *') ||
-    e.target.matches('.nav-button')
-  )
-    return;
+$body.onclick = () => {
   $calendar.classList.remove('active');
 };
